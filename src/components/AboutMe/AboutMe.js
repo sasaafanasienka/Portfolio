6466ea7f809465b68c0c335d055a65307a96fe1b
sasaafanasienka <mark0182@tut.by>
2021-03-20 21:33:54 +0300

    первый коммиит

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,13 @@
+import userpic from '../../images/userpic.jpg'
+import './AboutMe.sass'
+
+function AboutMe() {
+    return (
+        <div className='AboutMe'>
+            <img className='AboutMe__userpic' src={userpic}/>
+            <h2 className='AboutMe__title'>Александр Афанасенко</h2>
+            <h3 className='AboutMe__subtitle'>Junior frontend-разработчик</h3>
+        </div>
+    )
+}
+export default AboutMe
