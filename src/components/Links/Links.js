1c48b8582372de833079d101a88b7b3ab01db13a
sasaafanasienka <mark0182@tut.by>
2021-03-21 20:52:46 +0300

    добавлены ссылки

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -1,10 +1,15 @@
 import telegram from '../../images/logo_telegram_light.svg'
+import linkedin from '../../images/logo_linkedin_light.svg'
+import github from '../../images/logo_github_light.svg'
 import './Links.sass'
 
 function Links() {
     return (
         <div className='Links'>
             <a href='https://t.me/sanya2020327'><img className='Links__icon' src={telegram}></img></a>
+            <a href='https://www.linkedin.com/in/александр-афанасенко-bb672b15a/'><img className='Links__icon' src={linkedin}></img></a>
+            <a href='https://github.com/sasaafanasienka'><img className='Links__icon' src={github}></img></a>
+            <a className='Links__button' href='https://cloud.mail.ru/public/pGDt/DTBYUnmk7'>СКАЧАТЬ РЕЗЮМЕ</a>
         </div>
     )
 }
