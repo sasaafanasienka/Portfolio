80480084e9357cf856fb3df1811f1f96f61274f9
sasaafanasienka <mark0182@tut.by>
2021-04-18 22:52:42 +0300

    typescript added in ig popup content

--- src/components/BigPopup/BigPopup.tsx
+++ src/components/BigPopup/BigPopup.tsx
@@ -7,21 +7,26 @@ type popupProps = {
     title: string;
 }
 
+type projectsType = {
+    [key: string]: string
+}
+
 const BigPopup:React.FC<popupProps> = ({title}) => {
 
-    // function closePopup() {
-    //     document.querySelector('.BigPopup').classList.remove('BigPopup_active')
-    // }
-
+    function closePopup() {
+        document.querySelector('.BigPopup')!.classList.remove('BigPopup_active')
+    }
+    
     function closePopupByKeyboard(event: React.KeyboardEvent) {
         console.log(event)
+        document.querySelector('.BigPopup')!.classList.remove('BigPopup_active')
     }
     
     return (
         <div className='BigPopup' onKeyPress={closePopupByKeyboard}>
-            {/* <button className='BigPopup__close' onClick={closePopup}><img src={closeIcon}></img></button> */}
+            <button className='BigPopup__close' onClick={closePopup}><img src={closeIcon} alt='close popup button'></img></button>
             <div className='BigPopup__content'>
-                {/* {projects[title]} */}
+                {title ? (projects[title] as projectsType).info : null}
             </div>
         </div>
     )
